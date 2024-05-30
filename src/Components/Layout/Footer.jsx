381b882703586e61ece781db84573d9561726513
Ahmed Ghodhbeni <ahmed.godhbeni@gmail.com>
2024-05-30 15:12:56 +0200

    build simple header and footer

---
+++ src/Components/Layout/Footer.jsx
@@ -0,0 +1,17 @@
+const Footer = () => {
+    return (
+      <div
+        style={{
+          color: "gray",
+          marginTop: "10px",
+          borderTop: "1px solid #555",
+          textAlign: "center",
+        }}
+      >
+        Happy Coding! @ContactDev
+      </div>
+    );
+  };
+  
+  export default Footer;
+  
